77a0a5906af59e2d633640bc503e5f22f469d28b
Xing Kimball <xingkimb@gmail.com>
2023-07-15 19:40:01 -0400

    feat: show both output

--- Agent1.py
+++ Agent1.py
@@ -4,7 +4,7 @@ from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains import LLMChain, SequentialChain
 
 
 os.environ["OPENAI_API_KEY"] = apikey
@@ -23,19 +23,25 @@ title_template = PromptTemplate(
 # templates2
 script_template = PromptTemplate(
     input_variables=["title"],
-    template="give me a quick summary of the book {title}",
+    template="give me a quick summary of the book in 30 words{title}",
 )
 
 # llms
 llm = OpenAI(temperature=0.9)
 # chain
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
-sequential_chain = SimpleSequentialChain(
-    chains=[title_chain, script_chain], verbose=True
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key="title")
+script_chain = LLMChain(
+    llm=llm, prompt=script_template, verbose=True, output_key="script"
+)
+sequential_chain = SequentialChain(
+    chains=[title_chain, script_chain],
+    input_variables=["topic"],
+    output_variables=["title", "script"],
+    verbose=True,
 )
 
 # Get response from input
 if prompt:
-    response = sequential_chain.run(prompt)
-    st.write(response)
+    response = sequential_chain({"topic": prompt})
+    st.write(response["title"])
+    st.write(response["script"])
